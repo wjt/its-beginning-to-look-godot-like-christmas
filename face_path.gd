0359aa303ef3cc0444a58b3c408ca8c700cf8f91
Will Thompson <wjt@endlessos.org>
2024-12-10 10:51:32 +0000

    More faces

--- face_path.gd
+++ face_path.gd
@@ -1,32 +1,23 @@
 extends Path2D
 
-enum Direction {
-	FORWARDS,
-	BACKWARDS,
-}
-
 @onready var _follow = $FacePathFollow
-@onready var _faces = [
-	[$FacePathFollow/Rob, Direction.FORWARDS],
-	[$FacePathFollow/Rodrigo, Direction.BACKWARDS],
-	[$FacePathFollow/Stephen, Direction.FORWARDS],
-]
+@onready var _faces: Array[Node] = _follow.get_children()
+var _i = 0
 const DURATION = 20.0
 
 func _start_a_face():
 	var tween = get_tree().create_tween()
-	var x = _faces.pick_random()
-	var sprite: Sprite2D = x[0]
-	var direction = x[1]
+	var sprite = _faces[_i]
+	_i = (_i + 1) % len(_faces)
 	sprite.show()
-	match direction:
-		Direction.FORWARDS:
-			_follow.progress_ratio = 0.0
-			tween.tween_property(_follow, "progress_ratio", 1.0, DURATION)
-		Direction.BACKWARDS:
-			_follow.progress_ratio = 1.0
-			tween.tween_property(_follow, "progress_ratio", 0.0, DURATION)
+	if true:
+		_follow.progress_ratio = 0.0
+		tween.tween_property(_follow, "progress_ratio", 1.0, DURATION)
+	else:
+		_follow.progress_ratio = 1.0
+		tween.tween_property(_follow, "progress_ratio", 0.0, DURATION)
 	tween.tween_callback(sprite.hide)
+	tween.tween_callback($Timer.start)
 
 
 func _on_timer_timeout() -> void:
