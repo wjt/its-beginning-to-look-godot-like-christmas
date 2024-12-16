a5f781d032b1158e3908454e882410a654110796
Will Thompson <wjt@endlessos.org>
2024-12-16 11:00:49 +0000

    Improve keyboard/joypad switch in control legend

--- scenes/control_legend/control_legend.gd
+++ scenes/control_legend/control_legend.gd
@@ -3,14 +3,11 @@ extends Control
 @onready var _grid: GridContainer = $PanelContainer/Grid
 
 func _ready() -> void:
-	_toggle_joypad_controls(false)
 	Input.joy_connection_changed.connect(_on_joy_connection_changed_cb)
 
 func _toggle_joypad_controls(connected: bool):
-	_grid.columns = (3 if connected else 2)
-	for child in $PanelContainer/Grid.get_children():
-		if child.is_in_group("joypad"):
-			child.visible = connected
+	for child in get_tree().get_nodes_in_group("joypad"):
+		child.visible = connected
 
 func _on_joy_connection_changed_cb(_device: int, _connected: bool):
 	_toggle_joypad_controls(not Input.get_connected_joypads().is_empty())
